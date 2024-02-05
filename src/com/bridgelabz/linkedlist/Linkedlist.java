3fa8f78ed863fee74d97960de62944b215927b4b
Dhanush Kumar <dk652003@gmail.com>
2024-02-05 21:10:19 +0530

    Deleting the first element in the Linkedlist

--- src/com/bridgelabz/linkedlist/Linkedlist.java
+++ src/com/bridgelabz/linkedlist/Linkedlist.java
@@ -1,46 +1,39 @@
 package com.bridgelabz.linkedlist;
 
-class Node {
+class Node{
     int data;
+    // next is a reference variable used to refer the next node
     Node next;
-
-    public Node(int data) {
+    public Node(int data){
         this.data = data;
         this.next = null;
     }
 }
-
 public class Linkedlist {
     Node head;
-
-    public Linkedlist() {
+    public Linkedlist(){
         head = null;
     }
-
-    public void insert(int val, int insertAfter) {
+    public void insert(int val){
         Node newNode = new Node(val);
-        if (head == null) {
+        // List is empty
+        if(head == null){
             head = newNode;
-        } else {
-            Node current = head;
-            while (current != null && current.data != insertAfter) {
-                current = current.next;
-            }
-            if (current != null) {
-                newNode.next = current.next;
-                current.next = newNode;
-            }
+        }
+        // List is not empty
+        else{
+            newNode.next = head;
+            head = newNode;
         }
     }
-
-    public void display() {
+    public void display(){
+        // change the head to next node to remove the first element
+        head = head.next;
         Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + "->");
+        while(temp != null){
+            System.out.print(temp.data+"->");
             temp = temp.next;
         }
-        System.out.println("null");
+        System.out.print("NULL");
     }
-
 }
-
