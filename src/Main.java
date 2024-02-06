7dc743b001dcf9345514cefeb4ab790b3854f156
Dhanush Kumar <dk652003@gmail.com>
2024-02-06 16:48:23 +0530

    Added Finding The Occurance Of Element

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-import com.bridgelabz.linkedlist.Linkedlist;
+import com.bridgelabz.linkedlist.MyLinkedlist;
 
 public class Main {
     public static void main(String[] args) {
-        Linkedlist list = new Linkedlist();
-        list.insert(70);
-        list.insert(30);
-        list.insert(56);
+        MyLinkedlist list = new MyLinkedlist();
+        list.insertAfter(56,0);
+        list.insertAfter(30,56);
+        list.insertAfter(40,30);
+        list.insertAfter(70,40);
         list.display();
-        System.out.println();
-        list.findposition(30);
+        list.search(40);
     }
 }
---
+++ src/com/bridgelabz/linkedlist/MyLinkedlist.java
@@ -0,0 +1,82 @@
+package com.bridgelabz.linkedlist;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class MyLinkedlist {
+    Node head;
+
+    public MyLinkedlist() {
+        head = null;
+    }
+    // Insert Node At The Beginning
+    public void insertAtBeginning(int val){
+        Node newNode = new Node(val);
+        // List is empty
+        if(head == null){
+            head = newNode;
+        }
+        // List is not empty
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+    //Insert Node At The End
+    public void append(int val){
+        Node newNode1 = new Node(val);
+        if(head == null){
+            head = newNode1;
+        }
+        else{
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode1;
+        }
+    }
+    // Insert a Node After A Certain Node
+    public void insertAfter(int val, int insertAfter) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current != null && current.data != insertAfter) {
+                current = current.next;
+            }
+            if (current != null) {
+                newNode.next = current.next;
+                current.next = newNode;
+            }
+        }
+    }
+    //Position In Which The Node Occur
+    public void search(int val){
+        int count = 0;
+        Node current = head;
+        while(current.data != val){
+            current = current.next;
+            count+=1;
+        }
+        System.out.println(count);
+
+    }
+    public void display() {
+        //head = head.next; To delete the first node
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+}
